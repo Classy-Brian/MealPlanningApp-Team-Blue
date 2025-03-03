12ee9d7c237a87caedf9dba0cadbc63b2ced2278
rie01 <lilacqlo2496@gmail.com>
2025-03-02 18:06:19 -0800

    current

--- frontend/ByteMe/app/(tabs)/_layout.jsx
+++ frontend/ByteMe/app/(tabs)/_layout.jsx
@@ -7,6 +7,7 @@ export default function TabLayout() {
     return (
         <Tabs 
             tabBar={props=> <TabBar {...props} />}
+            screenOptions={{headerShown: false}}
         >
             <Tabs.Screen
                 name="index"
--- frontend/ByteMe/app/_layout.jsx
+++ frontend/ByteMe/app/_layout.jsx
@@ -1,12 +1,33 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
 import { Stack, Tabs } from 'expo-router'
 
 
+// function HeaderLogo = () => {
+//   return (
+//     <View style={{flex: 1, flexDirection: 'row', flexWrap: 'wrap', alignItems:
+//       <Image 
+//     }}>
+
+//     </View>
+//   )
+// }
+
 const _layout = () => {
   return (
-    <Stack screenOptions={{headerShown: false}}>
-        <Stack.Screen name="(tabs)" screenOptions={{headerShown: false}} />
+    <Stack >
+        <Stack.Screen name="(tabs)"
+          options={{
+            headerShown: true,
+            title: 'ByteMe',
+            headerStyle: {
+              backgroundColor: '#1F508F'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+            }} />
     </Stack>
   )
 }
