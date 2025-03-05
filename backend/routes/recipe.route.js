ffbb95ca415659c2ea9ba59ada916a7986f87c94
JorgeM1110 <mejiapity404@gmail.com>
2025-03-04 21:51:00 -0800

    reset

--- backend/routes/recipe.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import { getRecipes } from "../controllers/recipe.controller.js";
-
-const router = express.Router();
-
-router.get("/", getRecipes);
-
-export default router;
