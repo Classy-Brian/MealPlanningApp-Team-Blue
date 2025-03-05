8c4ed10323188c8bbc399dd9ede6c9ff5554950d
JorgeM1110 <mejiapity404@gmail.com>
2025-03-05 08:12:53 -0800

    Reapply "Merge remote-tracking branch 'origin/Testing' into Jorge"
    This reverts commit 1d8e7a30fc73e29e5dc628bb3bbf5e51b6db5ef1.

---
+++ frontend/ByteMe/app/index.jsx
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Link } from "expo-router"
+
+const WelcomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome Screen</Text>
+      <Link href={"/(start)/signup"} asChild>
+        <Text>Sign Up</Text>
+      </Link>
+      <Link href={"/(start)/login"} asChild>
+        <Text>Login</Text>
+      </Link>
+      <Link href={"/(tabs)/home"}>
+        <Text>Homepage</Text>
+      </Link>
+    </View>
+  )
+}
+
+export default WelcomeScreen
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
---
+++ frontend/ByteMe/components/TextColors.jsx
@@ -0,0 +1,8 @@
+export const textcolors = {
+    white: '#FFFFFF',
+    black: '#000',
+    lightgrey: '#989898',
+    red: '#FE6565',
+
+
+}
