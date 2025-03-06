d94b91d11efbbe28af3b2e654e9e486778755113
Classy-Brian <phanbrian101@gmail.com>
2025-03-05 22:29:58 -0800

    Restructured frontend

--- frontend/ByteMe/app/(header)/profile.jsx
+++ frontend/ByteMe/app/(header)/profile.jsx
@@ -1,12 +1,2 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
-const Profile = () => {
-  return (
-    <View>
-      <h1>Profile</h1>
-    </View>
-  )
-}
-
-export default Profile
+import ProfileScreen from "@/screens/ProfileScreen";
+export default ProfileScreen;
